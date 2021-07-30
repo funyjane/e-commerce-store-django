c5f08a181a0a572ee2ace5f761aff1442f7b82bc
WIN-NFEEIG0N8SU\Danila <danila.stasiuk@gmail.com>
2021-07-30 20:22:50 +0300

    init: models revision and adds requirements.txt

--- src/main/models.py
+++ src/main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Listing(models.Model):
+    title = models.CharField(blank=False, null=False, max_length=100)
+    description = models.TextField(blank=False, null=False)
+    category = models.ForeignKey(Category, null=False, on_delete=models.SET_NULL)
+    seller = models.ForeignKey(Owner, null=False, on_delete=models.SET_NULL)
+    created_at = models.DateField(auto_now_add=True)
+    edited_at = models.DateField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+
+
+class Category(models.Model):
+    pass
+
+
+class Owner(models.Model):
+    pass
+
+
+class Tag(models.Model):
+    pass
