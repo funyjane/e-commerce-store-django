8298b9a8ac07fb6debd03be918c2daf132122a30
Danila <danila.stasiuk@gmail.com>
2021-08-02 17:29:09 +0300

    feat: adds AbstractBaseListing model, creates Item, Car, Service with AbstractListing as parent, registres mentioned models in admin.py

--- src/main/admin.py
+++ src/main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from django.contrib.flatpages.models import FlatPage
 
 
@@ -7,6 +8,8 @@ from django.contrib.flatpages.admin import FlatpageForm as FlatpageFormOld
 
 from django import forms
 from ckeditor.widgets import CKEditorWidget
+
+from main.models import Category, Seller, Tag, Item, Car, Service, ArchiveListing
 
 
 class FlatpageForm(FlatpageFormOld):
@@ -23,3 +26,40 @@ class FlatPageAdmin(FlatPageAdminOld):
 
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, FlatPageAdmin)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "username",
+    )
+    list_display_links = (
+        "id",
+        "email",
+        "username",
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = [
+        "title",
+    ]
+    list_display = [
+        "title",
+    ]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Seller)
+admin.site.register(Car)
+admin.site.register(Item)
+admin.site.register(Service)
+admin.site.register(ArchiveListing)
