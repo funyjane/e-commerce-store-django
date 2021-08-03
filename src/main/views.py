c726e611a8f5d0209b9bf9a6d1115c1ae0c31c4b
Danila <danila.stasiuk@gmail.com>
2021-08-03 22:54:59 +0300

    feat: adds django-constance, enable maintenance window in index.html, adds user.is_authenticated welcome msg

--- src/main/views.py
+++ src/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from constance import config
 
 
 class IndexPageView(TemplateView):
     template_name = "pages/index.html"
+    turn_on_block = config.MAINTENANCE_MODE
+    extra_context = {"maintenance": turn_on_block}
