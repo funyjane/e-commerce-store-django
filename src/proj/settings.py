8a0c771a197aff248b8080ca416dbf62b54ed4cd
Danila <danila.stasiuk@gmail.com>
2021-08-09 18:05:46 +0300

    refactor: refactors static path using os module

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = "proj.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -87,9 +87,6 @@ TEMPLATES = [
     },
 ]
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 WSGI_APPLICATION = "proj.wsgi.application"
 
@@ -142,6 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 
 # Default primary key field type
