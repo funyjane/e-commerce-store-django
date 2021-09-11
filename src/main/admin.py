ab91da85df2bad1089c30e379be83bca1a602d6f
WIN-NFEEIG0N8SU\Danila <danila.stasiuk@gmail.com>
2021-09-11 16:09:45 +0300

    feat: custom management commands-archive, disable

--- src/main/admin.py
+++ src/main/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.contrib.admin.utils import quote
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin.views.main import ChangeList
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.contrib.flatpages.models import FlatPage
 
@@ -8,6 +12,7 @@ from django.contrib.flatpages.admin import FlatpageForm as FlatpageFormOld
 
 from django import forms
 from ckeditor.widgets import CKEditorWidget
+from sorl.thumbnail.admin import AdminImageMixin
 
 from main.models import (
     Category,
@@ -19,6 +24,7 @@ from main.models import (
     Service,
     ArchiveListing,
     SMSLog,
+    Picture,
 )
 
 
@@ -51,6 +57,55 @@ class TagAdmin(admin.ModelAdmin):
     list_display = [
         "title",
     ]
+
+
+class PictureAdminInline(AdminImageMixin, admin.TabularInline):
+    model = Picture
+
+
+@admin.action(description="Archives selected listings")
+def make_inactive(modeladmin, request, queryset):
+    queryset.update(is_active=False)
+
+
+@admin.action(description="Activate selected listings")
+def make_active(modeladmin, request, queryset):
+    queryset.update(active=True)
+
+
+@admin.action(description="Mark lisitngs as sold")
+def make_sold(modeladmin, request, queryset):
+    queryset.update(sold=True)
+
+
+@admin.action(description="Mark lisitngs as not sold")
+def make_not_sold(modeladmin, request, queryset):
+    queryset.update(is_sold=False)
+
+
+class AdSubModelsAdmin(AdminImageMixin, admin.ModelAdmin):
+    list_display = (
+        "title",
+        "seller",
+        "sold",
+        "active",
+    )
+    ordering = ["created_at", "edited_at"]
+    list_filter = (
+        "sold",
+        "active",
+        "created_at",
+        "edited_at",
+        ("tags", admin.RelatedOnlyFieldListFilter),
+    )
+    inlines = [PictureAdminInline]
+    search_fields = [
+        "name",
+        "seller__first_name",
+        "seller__last_name",
+        "seller__username",
+    ]
+    actions = [make_inactive, make_active, make_sold, make_not_sold]
 
 
 admin.site.register(Seller)
